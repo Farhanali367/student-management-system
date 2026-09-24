162789cd6e4b727f25f458026f63f330299abb0e
Mohd Farhan <mohdfarhan098ali@gmail.com>
2026-09-24 14:56:36 +0530

    Fix Github Pages CORS HTTPS

--- backened/main.py
+++ backened/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500","http://farhanali367.github.io",
+        "http://localhost:5500","https://farhanali367.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
